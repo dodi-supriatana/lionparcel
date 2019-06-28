62a904f3c4057ecb168af8aa7eae4b23302344f7
dodi to <dodisupriatna8@gmail.com>
2019-06-28 16:23:26 +0700

    update kode post label

--- application/modules/kodepost/controllers/kodepost.php
+++ application/modules/kodepost/controllers/kodepost.php
@@ -31,7 +31,7 @@ class kodepost extends MX_Controller
         // search data
         $kode=$this->input->post('kecamatan');
        
-        $data = $this->db->query("SELECT kode_pos,3lc FROM wilayah w WHERE kecamatan='$kode' GROUP BY kecamatan")->result();
+        $data = $this->db->query("SELECT kode_pos as label,3lc as value FROM wilayah w WHERE kecamatan='$kode' GROUP BY kecamatan")->result();
         $this->djson(
             array(
                 "status" => "200",
