bb531ef0bdbb0b951f747864542a8b1cdf35d508
dodi to <dodisupriatna8@gmail.com>
2019-06-28 14:35:08 +0700

    revisi kab kota

--- application/modules/kecamatan/controllers/kecamatan.php
+++ application/modules/kecamatan/controllers/kecamatan.php
@@ -31,7 +31,7 @@ class kabkota extends MX_Controller
         // search data
         $provinsi=$this->input->post('provinsi');
        
-        $data = $this->db->query("SELECT CONCAT(w.type,' ',w.kab_kota) as kota FROM wilayah w WHERE provinsi='$provinsi' GROUP BY kota")->result();
+        $data = $this->db->query("SELECT CONCAT(w.type,' ',w.kab_kota) as kota FROM wilayah w WHERE provinsi='$provinsi'")->result();
         $this->djson(
             array(
                 "status" => "200",
