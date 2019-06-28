c6753ec51e211eca8bff199b001eddc699bb37cd
dodi to <dodisupriatna8@gmail.com>
2019-06-28 15:13:43 +0700

    update kodepost

---
+++ application/modules/kodepost/controllers/kodepost.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class kodepost extends MX_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        //  if($this->session->userdata('status') != "login"){
+        //     redirect(base_url("/login/signOut"));
+        // }
+        //  $this->load->model('M_channel');
+        // $this->load->model('MGmenu');
+        // $this->load->model('mprojectlist');
+    }
+
+    private function djson($value = array())
+    {
+        $json = json_encode($value);
+        $this->output->set_header("Access-Control-Allow-Origin: *");
+        $this->output->set_header("Access-Control-Expose-Headers: Access-Control-Allow-Origin");
+        $this->output->set_status_header(200);
+        $this->output->set_content_type('application/json');
+        $this->output->set_output($json);
+    }
+
+    public function get_kodepost()
+    {
+
+        // search data
+        $kode=$this->input->post('kecamatan');
+       
+        $data = $this->db->query("SELECT kode_pos FROM wilayah w WHERE kecamatan='$kode' GROUP BY kecamatan")->result();
+        $this->djson(
+            array(
+                "status" => "200",
+                "data" => $data 
+            )
+        );
+    }
+}
