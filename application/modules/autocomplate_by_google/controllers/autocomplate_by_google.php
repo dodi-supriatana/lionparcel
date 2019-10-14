b75808e30af0dd881773c902d863e9e3d4d11dc5
dodi to <dodisupriatna8@gmail.com>
2019-10-14 18:54:40 +0700

    update autocomplate_by_google

--- application/modules/autocomplate_by_google/controllers/autocomplate_by_google.php
+++ application/modules/autocomplate_by_google/controllers/autocomplate_by_google.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class autocomplate extends MX_Controller
+class autocomplate_by_google extends MX_Controller
 {
 
     function __construct()
@@ -25,76 +25,36 @@ class autocomplate extends MX_Controller
         $this->output->set_output($json);
     }
 
-    public function get_autocomplate()
-    {
-        $word = $this->input->post('word');
-        if (!empty($word)) {
-            $query = $this->db->query("SELECT w.3lc as id,CONCAT( w.kelurahan, ', ',w.kecamatan, ', ', w.type ,' ', w.kab_kota, ', ',w.provinsi) as name FROM wilayah w WHERE CONCAT( w.kelurahan, ', ',w.kecamatan,' ', w.kab_kota, ' ',w.provinsi) like '%" . $word . "%' GROUP BY kecamatan limit 10 ");
-
-
-            $this->djson(
-                array(
-                    "status" => "200",
-                    "data" => $query->result()
-                )
-            );
-        } else {
-            $this->djson(
-                array(
-                    "status" => "200",
-                    "data" => []
-                )
-            );
-        }
-    }
-
-    public function get_address()
-    {
-        $latitude = $this->input->post('latitude');
-        $longitude = $this->input->post('longitude');;
-        $geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $latitude . ',' . $longitude . '&sensor=false&key=AIzaSyCfY9TPZ31i6nu-oTLQWjuHaIt5dbc86o4&sensor=false');
-        $output = json_decode($geocode);
-        $formattedAddress = @$output->results[0]->formatted_address;
-        $this->djson(
-            array(
-                "status" => "200",
-                "data" => $formattedAddress
-            )
-        );
-    }
-
-    public function get_autocomplate_kecamatan()
-    {
-        $word = $this->input->post('word');
-        if (!empty($word)) {
-            $query = $this->db->query("SELECT w.kecamatan as id,CONCAT(w.kecamatan, ', ', w.type ,' ', w.kab_kota, ', ',w.provinsi) as name FROM wilayah w WHERE CONCAT(w.kecamatan,' ', w.kab_kota, ' ',w.provinsi) like '%" . $word . "%' GROUP BY kecamatan limit 10 ");
-
-
-            $this->djson(
-                array(
-                    "status" => "200",
-                    "data" => $query->result()
-                )
-            );
-        } else {
-            $this->djson(
-                array(
-                    "status" => "200",
-                    "data" => []
-                )
-            );
-        }
-    }
-
     public function get_autocomplate_address(){
         $address = $this->input->post('address');
 
-		$geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&key=AIzaSyBmtZNz9aMpD-tDGdjX_ZmvkdCLe8orp7U&sensor=false');
-		$output = json_decode($geocode);
-		print_r($output);
-		die();
-		$lat = @$output->results[0]->geometry->location->lat;
-		$lng = @$output->results[0]->geometry->location->lng;
+		// $geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&key=AIzaSyBmtZNz9aMpD-tDGdjX_ZmvkdCLe8orp7U&sensor=false');
+		$geocode = file_get_contents('https://maps.googleapis.com/maps/api/place/autocomplete/json?input='. urlencode($address) .'&types=geocode&language=en&key=AIzaSyBnlCirhyEFsqNA1YDKyBBicWWb3T37ZBk');        
+        $output = json_decode($geocode);
+        
+        $i=0;
+        // $jalan=array();
+        foreach ($output->predictions as $key) {
+            $data[$i]['tempat']=@$output->predictions[$i]->description;
+            $address2 = @$output->predictions[$i]->description;
+            // die($address);
+            $geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address2) . '&key=AIzaSyBmtZNz9aMpD-tDGdjX_ZmvkdCLe8orp7U&sensor=false');
+            $outputjalan = json_decode($geocode);
+            $data[$i]['lat'] = @$outputjalan->results[0]->geometry->location->lat;
+            $data[$i]['lng'] = @$outputjalan->results[0]->geometry->location->lng;
+            $i++;
+        }
+        
+
+        // echo json_encode($jalan,JSON_PRETTY_PRINT);
+        
+        $this->djson(
+            array(
+                "status" => "200",
+                // "data"=>$header,
+                "history" => $data
+            )
+        );
 
 
     }
